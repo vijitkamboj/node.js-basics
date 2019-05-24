9d050a07c7f8074122d3dbaa682b83a05f46c200
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-24 22:11:02 +0530

    some adjustments

--- expressServer.js
+++ expressServer.js
@@ -3,14 +3,22 @@ const express = require('express');
 const app = express()
 
 const user ={
-    name: "Vijit"
+    name: "Vijit",
+    l_name : "Kamboj"
 }
-app.get('/user' , (req,res) => {
-    res.send(user)              // automatically recognizes the content-type
-})
+
 app.get('/' , (req,res) => {
     res.send("<h1> Hello World !!!! ,</h1>")
 
 })
+
+app.get('/user' , (req,res) => {
+    res.send("Getting Profiles")              // automatically recognizes the content-type
+})
+
+app.post('/user' , (req,res) => {
+               
+})
+
 app.listen(3000)
 
