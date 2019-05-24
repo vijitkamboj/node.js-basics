6495beb47a43edfa10c5332f1de915ffc39b6ed1
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-24 15:01:34 +0530

    generated first JSON response

---
+++ server.js
@@ -0,0 +1,15 @@
+const http  = require('http');
+const ab = {
+    a:4,
+    b:5
+}
+
+const server = http.createServer((request , response) => {
+    // console.log("header",request.headers);
+    console.log("urls",request.url);
+    // console.log("method",request.method)
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify(ab))
+})
+
+server.listen(3000)
